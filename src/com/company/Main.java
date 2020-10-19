0b1ae0f49ada908594edc7440ebb8eb726a432e9
U-Toshiba\Mateusz <mateuszwolniszewski@wp.pl>
2020-10-19 20:15:26 +0200

    Zliczanie osób

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,47 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
 }
 
-class PeopleStats {
+        class Person {
+            private final String firstName;
+            private final String lastName;
 
+            public Person(String firstName, String lastName) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+
+            public String getLastName() {
+                return lastName;
+            }
+
+            public String getFirstName() {
+                return firstName;
+            }
+        }
+
+    class PeopleStats {
+        private final List<Person> people;
+
+        public PeopleStats(Path inputFilePath) {
+            try {
+                people = Files.lines(inputFilePath)
+                        .map(line -> line.split("\t"))
+                        .map(chunks -> new Person(chunks[0], chunks[1]))
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+
+            }
 }
-
-class Person {
-    private final String firstName;
-    private final String lastName;
-
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    public String getLastName(){return lastName;}
-
-    public String getFirstName() {
-        return firstName;
-    }
 }
