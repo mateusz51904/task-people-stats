17c05bb06043b1ef9383b2ae1294e809d49ff51e
U-Toshiba\Mateusz <mateuszwolniszewski@wp.pl>
2020-10-19 20:19:57 +0200

    Wersja ostateczna

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,8 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
-
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
         System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
     }
 }
@@ -52,6 +53,12 @@ class Main {
 
         public long count() {
             return people.size();
+        }
+        public long countUniqueLastNames() {
+            return people.stream()
+                    .map(Person::getLastName)
+                    .distinct()
+                    .count();
         }
     }
 
